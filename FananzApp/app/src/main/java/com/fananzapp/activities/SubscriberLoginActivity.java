4f467a112ec89431c82324e5c2f3d0c0a27fa554
AkshayVGadhave <akshay@vibeosys.com>
2016-12-26 18:08:05 +0530

    add login screens
    add login screens

---
+++ FananzApp/app/src/main/java/com/fananzapp/activities/SubscriberLoginActivity.java
@@ -0,0 +1,15 @@
+package com.fananzapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fananzapp.R;
+
+public class SubscriberLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subscriber_login);
+    }
+}
