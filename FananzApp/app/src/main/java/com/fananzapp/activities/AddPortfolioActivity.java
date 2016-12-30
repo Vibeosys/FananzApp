f397f060ffb5e0a7dc61ed94ffeaa450c3d65f44
AkshayVGadhave <akshay@vibeosys.com>
2016-12-30 13:41:00 +0530

    Add portfolio screens
    Add portfolio screens

---
+++ FananzApp/app/src/main/java/com/fananzapp/activities/AddPortfolioActivity.java
@@ -0,0 +1,15 @@
+package com.fananzapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fananzapp.R;
+
+public class AddPortfolioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_portfolio);
+    }
+}
