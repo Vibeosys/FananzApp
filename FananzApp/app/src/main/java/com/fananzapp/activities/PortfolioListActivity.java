6532b28345fed0890e87c07ba5cde33c60520080
AkshayVGadhave <akshay@vibeosys.com>
2016-12-30 14:11:49 +0530

    UI Changes
    UI Changes

--- FananzApp/app/src/main/java/com/fananzapp/activities/PortfolioListActivity.java
+++ FananzApp/app/src/main/java/com/fananzapp/activities/PortfolioListActivity.java
@@ -1,7 +1,9 @@
 package com.fananzapp.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.fananzapp.R;
 
@@ -11,5 +13,10 @@ public class PortfolioListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portfolio_list);
+    }
+
+    public void addPortFolio(View v) {
+        Intent iLogin = new Intent(getApplicationContext(), AddPortfolioActivity.class);
+        startActivity(iLogin);
     }
 }
